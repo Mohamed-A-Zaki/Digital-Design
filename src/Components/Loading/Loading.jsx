34cc28d146e3d5a62cfef676e9a9d7d5964244ef
Mohamed-A-Zaki <mohamedzaki385@gmail.com>
2023-01-24 02:00:17 +0200

    upload

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./Loading.scss";
+import { GridLoader  } from "react-spinners";
 
 const Loading = () => {
   return (
-    <div className="loading d-flex align-items-center justify-content-center position-absolute bg-dark">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className="loading w-100 h-100 d-flex align-items-center justify-content-center position-absolute top-0 start-0 bg-dark">
+      <GridLoader  color="#f1b74d" />
     </div>
   );
 };
